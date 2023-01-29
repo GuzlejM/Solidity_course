38d6a1eb1a7128e15a6b35334ce3ecf713cf9568
root <root@MSI.localdomain>
2023-01-29 18:07:25 +0100

    create person, get person

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,17 +2,38 @@
 
 pragma solidity ^0.8.8;
 
-contract SipleStorage {
+contract SimpleStorage {
 
     // unit8, unit124, uint256
     uint256 public favoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
-      favoriteNumber = _favoriteNumber;
-      _favoriteNumber = _favoriteNumber + 1;
+    struct People {
+        uint256 favoriteNumber;
+        string name;
     }
 
-    function retrieve() public view returns(uint256){
+    People[] public people; 
+
+    function store(uint256 _favoriteNumber) public {
+      favoriteNumber = _favoriteNumber;
+      retrieve();
+    }
+
+    function retrieve() public view returns (uint256){
         return favoriteNumber;
     } 
-} 
+    
+    //view, pure
+    function add() public pure returns (uint256) {
+      return(1 + 1);
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        people.push(newPerson);
+    }
+
+    function getAll() public view returns (People[] memory) {
+        return people;
+    }
+}

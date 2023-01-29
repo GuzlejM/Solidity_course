77d095b6c4dcf1d35d312dd1c992f333d263441f
root <root@MSI.localdomain>
2023-01-29 17:18:17 +0100

    retrieve function

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,9 +5,14 @@ pragma solidity ^0.8.8;
 contract SipleStorage {
 
     // unit8, unit124, uint256
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
       favoriteNumber = _favoriteNumber;
+      _favoriteNumber = _favoriteNumber + 1;
     }
-}
+
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    } 
+} 
